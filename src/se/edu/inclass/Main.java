c267da2d97703e791c0a8bd12af7d7603f78b31c
kangxinwang <juliawangkangxin@gmail.com>
2021-02-19 17:18:28 +0800

    add methods using stream

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,11 +16,12 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//
+//        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        printData(tasksData);
+//        printDataWithStreams(tasksData);
         printDeadlinesUsingStreams(tasksData);
         System.out.println("Total number of deadlines (using stream): "
                             + countDeadlinesWithStreams(tasksData));
@@ -35,11 +36,24 @@ public class Main {
         }
         return count;
     }
+    private static int countDeadlinesWithStreams(ArrayList <Task> tasksData){
+        int count = (int) tasksData.stream()
+                .filter((t)-> t instanceof Deadline) //predicate
+                .count();
+        return count;
+
+
+    }
 
     public static void printData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             System.out.println(t);
         }
+    }
+    public static void printDataWithStreams(ArrayList<Task> tasksData){
+        System.out.println("Printing tasks data using stream");
+        tasksData.stream()
+                .forEach(System.out::println);
     }
 
     public static void printDeadlines(ArrayList<Task> tasksData) {
